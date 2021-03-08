f8714ebc3fd6ae685121450744d83bc1ca786b80
MoriyaTzubara <m0533151327@gmail.com>
2021-03-08 20:51:03 +0200

    Queries

--- SQL/QUERIES.sql
+++ SQL/QUERIES.sql
@@ -1,7 +1,22 @@
-SELECT *
-FROM PERSON, PARAMEDIC 
-WHERE YearsOfExperience > 5;
+/*
+    All the ambulances that each of the drivers 
+    who are also volunteers have driven in the last year
+*/
 
-SELECT *
-FROM CALLHELP, TEAM
-WHERE 
+
+/*
+    All dispatchers whose number of calls received
+    in the past year is at least 100
+*/
+
+
+/*
+    All calls that sent to them at least one paramedic
+    with more than 10 years of experience
+*/
+
+
+/*
+    All ambulances sent by a dispatcher
+    that is also a paramedic with at least 20 years of experience
+*/
