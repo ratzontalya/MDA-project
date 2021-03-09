66ce7a9fc108ea6dbcdcdc1e1571abe3e47442af
MoriyaTzubara <m0533151327@gmail.com>
2021-03-09 19:36:23 +0200

    Queries

--- SQL/QUERIES.sql
+++ SQL/QUERIES.sql
@@ -23,6 +23,19 @@
     that is also a paramedic with at least 20 years of experience
 */
 
+
+/*
+    Delete the oldest dispatcher
+*/
+
+
+/*
+    Update the max YEARSOFEXPERIENCE to be 50:
+    If there is paramedic with YEARSOFEXPERIENCE grather than 50 - 
+    update it to be 50
+*/
+
+
 /*
 	Give me all the teams with the number of 
     the paramedics and volunteers
@@ -41,26 +54,50 @@ GROUP BY TEAMID;
     CallId, numVolunteers, numParamedics
 */
 
+SELECT CALLID, COUNT(DISTINCT PARAMEDICID), COUNT(DISTINCT VOLUNTEERID)
+FROM  CALLHELP NATURAL JOIN SENDTEAM NATURAL JOIN PARTICIPANTPARAMEDIC  NATURAL JOIN PARTICIPANTVOLUNTEER
+GROUP BY CALLID;
+
 /*
 	Give me all the ambulances that where used for 
     more than 50 times.
 */
 
+SELECT AMBULANCEID, COUNT(*)
+FROM TEAM
+GROUP BY AMBULANCEID
+HAVING COUNT(*) > 50;
+
 /*
 	I want the EXCELLENT paramedic which participated
-    in more than 20 calls or
+    in more than 30 calls or
     participated in "big events" which needed more 
     than 2 teams
 */
 
-/* UPDATE */
+SELECT FIRSTNAME, LASTNAME, COUNT(*)
+FROM PARTICIPANTPARAMEDIC JOIN PERSON ON (PERSONID = PARAMEDICID)
+GROUP BY PARAMEDICID, FIRSTNAME, LASTNAME
+HAVING COUNT(*) > 30;
+
+
 /*
    It is now the end of the year, so we need to add
    1 year of experience to every paramedic
 */
-/* DELETE */
+
+UPDATE PARAMEDIC
+SET YEARSOFEXPERIENCE = YEARSOFEXPERIENCE + 1;
+
 /*
    Because of a new law, people which are older than
    75 years can't be drive ambulance,
    so delete all of them.
 */
+
+DELETE FROM DRIVER
+WHERE DRIVERID IN (
+    SELECT DRIVERID
+    FROM DRIVER JOIN PERSON ON (DRIVERID = PERSONID)
+    WHERE BIRTHDATE < TO_DATE('01/01/1946', 'dd-mm-yyyy')
+    );
